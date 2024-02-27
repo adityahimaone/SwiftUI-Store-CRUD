0c91a11a2d9b74eaf0b45b8aa2a1ff4eb72ffbcd
adityahimaone <adityahimaone@gmail.com>
2024-02-27 20:04:42 +0700

    Initial Commit

---
+++ EStoreCRUD/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EStoreCRUD
+//
+//  Created by Aditya Himawan on 27/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EStoreCRUD/EStoreCRUDApp.swift
@@ -0,0 +1,17 @@
+//
+//  EStoreCRUDApp.swift
+//  EStoreCRUD
+//
+//  Created by Aditya Himawan on 27/02/24.
+//
+
+import SwiftUI
+
+@main
+struct EStoreCRUDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
